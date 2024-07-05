7ee3d8b6a1f22a878fb350e118678a6c8f1cfc00
anaGianno <blcoronado2004@gmail.com>
2024-07-06 11:48:27 +1200

    Add the starting files

---
+++ scripts/fence-cats.js
@@ -0,0 +1,15 @@
+const catProducts = [{
+  thumbnail: 'images/sus-cat1.jpg', 
+  thumbnailMini: {
+    secondThumbnail: 'images/sus-cat2.jpg',
+    thirdThumbnail: 'images/sus-cat3.jpg',
+    fourthThumbnail: 'images/sus-cat4.jpg',
+  }
+},{
+  thumbnail: 'images/green-cat1.jpg', 
+  thumbnailMini: {
+    secondThumbnail: 'images/green-cat2.jpg',
+  }
+},{
+  
+}];
