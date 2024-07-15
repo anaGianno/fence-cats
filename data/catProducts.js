54048847eefda08766f8551de7818ddecaff353d
anaGianno <blcoronado2004@gmail.com>
2024-07-15 12:04:53 +1200

    Use catProducts.js file

---
+++ data/catProducts.js
@@ -0,0 +1,51 @@
+const catProducts = [{
+    thumbnail: 'images/sus-cat1.jpg', 
+    thumbnailMini:
+    ['images/sus-cat2.jpg','images/sus-cat3.jpg','images/sus-cat4.jpg']
+  },{
+    thumbnail: 'images/green-cat1.jpg', 
+    thumbnailMini:
+    ['images/green-cat2.jpg']
+  },{
+    thumbnail: 'images/cool-cat1.jpg', 
+    thumbnailMini:
+    ['images/cool-cat2.jpg','images/cool-cat3.jpg']
+  },{
+    thumbnail: 'images/blue-cat1.jpg', 
+    thumbnailMini:
+    ['images/blue-cat2.jpg']
+  },{
+    thumbnail: 'images/curtain-cat1.jpg', 
+    thumbnailMini:
+    ['images/curtain-cat2.jpg']
+  },{
+    thumbnail: 'images/goat-cat1.jpg', 
+    thumbnailMini:
+    ['images/goat-cat2.jpg','images/goat-cat3.jpg']
+  },{
+    thumbnail: 'images/story-cat1.jpg', 
+    thumbnailMini:
+    ['images/story-cat2.jpg','images/story-cat3.jpg',
+      'images/story-cat4.jpg','images/story-cat5.jpg']
+  },{
+    thumbnail: 'images/sad-cat1.jpg', 
+    thumbnailMini:
+    ['images/sad-cat2.jpg','images/sad-cat3.jpg',]
+  },{
+    thumbnail: 'images/run-cat1.jpg', 
+    thumbnailMini:
+    ['images/run-cat2.jpg','images/run-cat3.jpg',
+      'images/run-cat4.jpg','images/run-cat5.jpg']
+  },{
+    thumbnail: 'images/murder-cat1.jpg', 
+    thumbnailMini:
+    ['images/murder-cat2.jpg','images/murder-cat3.jpg']
+  },{
+    thumbnail: 'images/black-cat1.jpg', 
+    thumbnailMini:
+    ['images/black-cat2.jpg']
+  },{
+    thumbnail: 'images/wary-cat1.jpg', 
+    thumbnailMini:
+    ['images/wary-cat2.jpg','images/wary-cat3.jpg']
+  }];
